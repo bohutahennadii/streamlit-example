6e4c74882b2088400ba23625ec3540233f016059
bohutahennadii <113023459+bohutahennadii@users.noreply.github.com>
2023-11-27 14:31:11 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,4 @@ import numpy as np
 import pandas as pd
 import streamlit as st
 
-
-num_points = st.selectbox('Select', [1,2,3])
-num_turns = st.slider('Slide me', min_value=0, max_value=10)
+url = st.text_input('Enter url')
